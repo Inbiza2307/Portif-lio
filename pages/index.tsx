df30286563d4eb772cc693b9986961bf85ebd9b0
Inbiza2307 <135852885+Inbiza2307@users.noreply.github.com>
2025-06-20 14:55:54 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-100">
       <div className="text-center">
-        <h1 className="text-4xl font-bold text-blue-600">Meu Site com Next.js + Tailwind</h1>
-        <p className="mt-4 text-gray-700">Hospedado com Vercel!</p>
+        <h1 className="text-4xl font-bold text-blue-600">Teste</h1>
+        <p className="mt-4 text-gray-700">Teste....</p>
       </div>
     </div>
   )
